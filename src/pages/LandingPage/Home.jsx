e13712c8ea6bb05d77071b8bb47091209374c9af
Emmy3131 <devinechild30032@gmail.com>
2026-02-16 18:33:10 +0000

    wip

--- src/pages/LandingPage/Home.jsx
+++ src/pages/LandingPage/Home.jsx
@@ -12,8 +12,8 @@ const Home = () => {
     <div className="overFloating-hidden">
       <Hero />
       <Trust /> 
-      {/* <HowItWorks />
-      <FeaturesAndSecurity />
+      <HowItWorks />
+      {/* <FeaturesAndSecurity />
       <TradingViewWidget />
       <Testimony />
       <FinalCTA />  */}
